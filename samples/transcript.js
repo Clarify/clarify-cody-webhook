04c76a494f842fe06e7fb6f8203813d428fd7133
Ivo Rothschild <ivo@clarify.io>
2017-09-13 18:23:18 -0400

    Added docs.

--- samples/transcript.js
+++ samples/transcript.js
@@ -11,8 +11,10 @@ FS.readFile(Path.resolve(__dirname, 'insights/sample_insight_transcript.json'), 
 
     var transcriptInsight = JSON.parse(data);
 
+    // Get a merged transcript (client and agent) from the transcript insight
     var transcript = ClarifyCody.transcript.transcriptForInsight(transcriptInsight);
 
+    // Generate formatted text for the transcript
     var text = ClarifyCody.transcript.textForTranscript(transcript);
 
     console.log(text);
